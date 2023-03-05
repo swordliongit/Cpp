3836b22354a04a15ec71aba96b9d7ede503c7c1c
Kılıçarslan <xswordlionx@gmail.com>
2023-03-05 23:33:13 +0300

    new file:   Arduino/PlatformIO/ESP32-Experimental/.gitignore 	new file:   Arduino/PlatformIO/ESP32-Experimental/.vscode/extensions.json 	new file:   Arduino/PlatformIO/ESP32-Experimental/include/README 	new file:   Arduino/PlatformIO/ESP32-Experimental/lib/README 	new file:   Arduino/PlatformIO/ESP32-Experimental/platformio.ini 	new file:   Arduino/PlatformIO/ESP32-Experimental/src/main.cpp 	new file:   Arduino/PlatformIO/ESP32-Experimental/test/README

---
+++ Arduino/PlatformIO/ESP32-Experimental/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+
+/***
+ * LED Blinking
+ */
+const int ledPin = 13;
+void setup() {
+    // setup pin 5 as a digital output pin
+    pinMode(ledPin, OUTPUT);
+}
+void loop() {
+    digitalWrite(ledPin, HIGH);	// turn on the LED
+    delay(500);	// wait for half a second or 500 milliseconds
+    digitalWrite(ledPin, LOW);	// turn off the LED
+    delay(500);	// wait for half a second or 500 milliseconds
+}
